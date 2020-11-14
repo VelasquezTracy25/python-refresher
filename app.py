cfab55f39a3b4a48a68f069c6865d5855fdf332c
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-14 15:18:25 -0600

    strings and variables lesson

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
-print("Hello World 🥰")
+# -*- coding: utf-8 -*-
+# print("Hello World 🥰")
 print("*" * 10)
+
+# PEP 8 standards
+x = 1
+
+student_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+print(student_count)
+
+course = "Python Programming"
+message = """
+Hi John, 
+
+This is an email. 
+
+Blah blah blah.
+
+"""
+
+print(len(course))
+print(course[0])
+print(course[-1])
