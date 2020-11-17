3543c5d6e1e77873c18be8cbc460cf80a5f43a56
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-17 09:27:17 -0600

    accessing items

--- data_structures.py
+++ data_structures.py
@@ -11,3 +11,17 @@ chars =list("Hello World")
 print(combined)
 print(numbers)
 print(len(chars))
+
+# Accessing items
+letters = ["a", "b", "c", "d"]
+print(letters)
+letters[0] = "A" 
+print(letters[-1])
+print (letters)
+print(letters[::2])
+
+numbers = list(range(20))
+# return list in reverse order
+print(numbers[::-1])
+# return every other item in list - 2, 4 ,6, 8...
+print(numbers[::2])
