7eda331bba3f1abefc3227a83949f51f6ef4e229
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-17 09:21:06 -0600

    lists

---
+++ data_structures.py
@@ -0,0 +1,13 @@
+# Lists
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+
+zeros = [0] * 5
+print(zeros)
+
+combined = zeros + letters
+numbers = list(range(20))
+chars =list("Hello World")
+print(combined)
+print(numbers)
+print(len(chars))
