485441a184fdea25b96a4ce41ddde72021fdf01b
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-17 09:34:10 -0600

    list unpacking

--- data_structures.py
+++ data_structures.py
@@ -25,3 +25,28 @@ numbers = list(range(20))
 print(numbers[::-1])
 # return every other item in list - 2, 4 ,6, 8...
 print(numbers[::2])
+
+# List Unpacking
+
+# Long way - not recommended
+# numbers = [1, 2, 3]
+# first = numbers[0]
+# second = numbers[1]
+# third = numbers[2]
+
+# Unpacking option
+numbers = [1, 2, 3]
+first, second, third = numbers
+print(first, second, third)
+
+# Unpacking with longer lists
+numbers = [1, 2, 3, 4, 4, 4, 4, 4]
+# Other is a list of all the items after the first two items
+first, second, *other = numbers
+print(first, second, other)
+
+# Unpacking with other in the middle of list
+numbers = [1, 2, 3, 4, 4, 4, 4, 9]
+# Other is a list of all the items after the first two items
+first, *other, last = numbers
+print(first, other, last)
